7a645975e65f98652365be45677d7aba10887367
wesley <wesleys.c190@gmail.com>
2026-05-05 15:37:44 -0300

    botões que modificam o array

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let lista_nome = ["cafe", "cafeina"]
 let adicionarF = document.getElementById('fn')
 let RmUltimo = document.getElementById('RMU')
 let RmPrimeiro = document.getElementById('RM')
-let ADDinicio = document. getElementById('AdI')
+let ADDinicio = document.getElementById('AdI')
+const nome = document.getElementById('nm')
 
-
-function renderizar(){
-     const lista_pagina = document.getElementById("lista")
-     lista_pagina.innerHTML``
-    for(let item of lista_nome){
+function renderizar() {
+    const lista_pagina = document.getElementById("lista")
+    lista_pagina.innerHTML = ''
+    for (let item of lista_nome) {
         let elemento = document.createElement('li')
         elemento.innerText = item
         lista_pagina.appendChild(elemento)
@@ -16,10 +16,28 @@ function renderizar(){
 }
 
 
-ADDinicio.addEventListener("click", function(){
-    const nome = textarea.value; 
-    
-} )
-document.addEventListener("DOMContentLoaded", ()=> {
+ADDinicio.addEventListener("click", function () {
+    const valor = nome.value;
+    lista_nome.unshift(valor);
+    renderizar();
+
 })
+adicionarF.addEventListener("click", function () {
+    const valor = nome.value;
+    lista_nome.push(valor);
+    renderizar();
+})
+
+RmPrimeiro.addEventListener("click", function () {
+    const valor = nome.value;   
+    lista_nome.pop(valor);
+    renderizar();
+
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderizar()
+
+})
+
 
