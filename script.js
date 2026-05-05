8a2e2ca16021ae2516223d610b5d290b84523455
wesley <wesleys.c190@gmail.com>
2026-05-05 15:10:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let lista_nome = ["cafe", "cafeina"]
+let adicionarF = document.getElementById('fn')
+let RmUltimo = document.getElementById('RMU')
+let RmPrimeiro = document.getElementById('RM')
+let ADDinicio = document. getElementById('AdI')
+
+
+function renderizar(){
+     const lista_pagina = document.getElementById("lista")
+     lista_pagina.innerHTML``
+    for(let item of lista_nome){
+        let elemento = document.createElement('li')
+        elemento.innerText = item
+        lista_pagina.appendChild(elemento)
+    }
+}
+
+
+ADDinicio.addEventListener("click", function(){
+    const nome = textarea.value; 
+    
+} )
+document.addEventListener("DOMContentLoaded", ()=> {
+})
+
