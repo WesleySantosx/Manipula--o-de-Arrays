cd587be2c10752418c3c978826b072f4f01fcf79
wesley <wesleys.c190@gmail.com>
2026-05-05 22:11:53 -0300

    atv5

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let RmUltimo = document.getElementById('RMU')
 let RmPrimeiro = document.getElementById('RM')
 let ADDinicio = document.getElementById('AdI')
 const nome = document.getElementById('nm')
+let nameM = document.getElementById('MAI')
 
 function renderizar() {
     const lista_pagina = document.getElementById("lista")
@@ -29,18 +30,44 @@ adicionarF.addEventListener("click", function () {
 })
 
 RmPrimeiro.addEventListener("click", function () {
-    const valor = nome.value;   
+    const valor = nome.value;
     lista_nome.shift(valor);
     renderizar();
 
 })
 
 RmUltimo.addEventListener("click", function () {
-    const valor = nome.value;   
+    const valor = nome.value;
     lista_nome.pop(valor);
     renderizar();
 
 })
+
+nameM.addEventListener("click", function(){
+    let resultado = " ";
+    lista_nome.forEach(function(nome){
+        resultado += nome.toUpperCase()+ " ";
+    })
+      document.getElementById("result").textContent= resultado; 
+})
+function contador() {
+    let contador = 0;
+    for (let i = 0; i < lista_nome.length; i++) {
+        if (lista_nome[i].length > 5) {
+            contador++;
+        }
+    }
+    document.getElementById("resultado").textContent= "total de nomes com mais de 5 letras:" + " " + contador; 
+}
+
+function filtrar(){
+    
+    const mai = lista_nome.filter(function(nome){
+        return nome.length>5;
+    })
+    document.getElementById("res").textContent = mai.join();
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
     renderizar()
